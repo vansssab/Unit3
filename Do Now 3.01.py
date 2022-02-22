2c18e1f2db6b7df7353d8122e8a46151fa26a9ef
Vanessa <vdbui25@students.bmchs.org>
2022-02-22 09:31:09 -0600

    Do Now 3.01

---
+++ Do Now 3.01.py
@@ -0,0 +1,21 @@
+'''
+#################
+Do Now 3.01
+#################
+1.  In your Console
+Type the following code:
+    1   import random
+    2   random.randint(0, 3)
+    3   random.randint(0, 3)
+    3   print(random.randint(0, 3))
+    4   print(random.randint(0, 3))
+    5   print(random.randint(0, 3))
+2.  In your Notebook
+Respond to the following
+    1.  What does randint() do?
+picks a random number from a range. random integer.
+    2.  What do the values 0 and 3 do? Try changing those numbers, rerun the program, and write down what changed.
+the limits for the code. the limit is changed.
+    3.  What is the difference between random.randint(0,3) and print(random.randint(0,3))?
+print- prints the random integer in the range. no print- changes random integer but doesn't print it.
+'''
