93a6dc1f4d6bdc1b57508c989b97e5a850894dae
Vanessa <vdbui25@students.bmchs.org>
2022-03-07 09:21:28 -0600

    Do now 3.04

---
+++ Do Now 3.04.py
@@ -0,0 +1,47 @@
+'''
+###############
+Do Now 3.04
+###############
+Quiz Announcement
+-----------------
+We will have a quiz tomorrow on Unit 2
+We will also have a quiz next Tuesday over Unit 3 so far.
+
+Discussion
+----------
+Is there any topic you would like to focus on and cover more of?  
+I will check the responses here and prepare to discuss tomorrow in class
+
+In your Notebook
+----------------
+Rank the following from easiest(1) to hardest(7):
+
+Importing built-in functions
+
+Using randint
+
+Abstraction/creating functions
+
+Passing int/str/float/bool arguments into functions
+
+Calling a function
+
+List syntax
+
+Return vs print
+
+Type the following in script mode
+---------------------------------
+my_list = ['a', 'b', 'c', 'd']
+# input: a list of strings
+# output: None
+def my_function(list_argument):
+    list_argument[0] = 'z'
+print(my_list)
+my_function(my_list)
+print(my_list)
+What did the program output and what is this program doing?
+
+
+Try writing a similar program but passing in integers instead of a list. What happens?
+'''
