19be5ca3dcb8a281638cb193bface255e5d6e590
Vanessa <vdbui25@students.bmchs.org>
2022-03-23 10:10:03 -0500

    Oregon Trail

--- Oregon Trail.py
+++ Oregon Trail.py
@@ -22,22 +22,77 @@ food = 500
 health = 5
 months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
 miles = 2000
-
-
+current_day = 1
+current_month = 3
 
 
 #options
 def travel():
     import random
+    global current_day
+    global current_month
+    global miles
+    days_passed = random.randint(3, 7)
+    current_day += days_passed
+    if current_day > 31 and months_with_31_days:
+        current_month += 1
+        current_day = 1
+    elif current_day > 30:
+        current_month += 1
+        current_day = 1
+    miles -= random.randint(30, 60)
+    print(f"{days_passed} have passed. \n"
+    f"You have traveled {miles} miles. \n"
+    f"The date is now {current_month}/{current_day}.")
 
-def rest():
+def rest(): 
     import random
+    global current_day
+    global current_month
+    global health
+    days_passed2= random.randint(2, 5)
+    current_day += days_passed2
+    if current_day > 31 and months_with_31_days:
+        current_month += 1
+        current_day = 1
+    elif current_day > 30:
+        current_month += 1
+        current_day = 1
+    health += 1
+    if health > 5:
+        print("Your health can not exceed 5 levels.")
+        health = 5
+    else:
+        print("Your health has increased. \n"
+        f"Your health is now {health}")
+    print(f"{days_passed2} have passed. \n"
+    f"The date is now {current_month}/{current_day}.")
 
 def hunt():
     import random
-
+    global food
+    global current_day
+    global current_month
+    food += 100
+    days_passed3= random.randint(2, 5)
+    current_day += days_passed3
+    if current_day > 31 and months_with_31_days:
+        current_month += 1
+        current_day = 1
+    elif current_day > 30:
+        current_month += 1
+        current_day = 1
+    print("You have gained 100 lbs of food. \n"
+    f"You now have {food} lbs of food. \n"
+    f"{days_passed3} have passed. \n"
+    f"The date is now {current_month}/{current_day}.")
+    
 def status():
-    pass
+    print("Your stats are: \n"
+    f"health = {health} health \n"
+    f"food = {food} lbs \n"
+    f"miles left = {miles} miles \n"
+    f"current date = {current_month}/{current_day}")
 
 def help():
     print("Your options are: \n"
@@ -49,7 +104,11 @@ def help():
     "quit: will end the game.")
 
 def quit():
-    print("I see you gave up. \n"
+    print("You want to give up? \n"
     "What a shame. \n"
     "You have died and vultures are now feeding on your dead remains.")
+
+#main program
+while not current_month == 12 and not current_day == 31:
+    pass
 
